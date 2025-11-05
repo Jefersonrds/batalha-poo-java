85d9adaff75254c93cdd6e00704c33539572a5a9
Jefersonrds <contatojefersonrds@gmail.com>
2025-11-04 23:39:57 -0300

    feat: adicionar classe Chefao

--- Batalha/src/Batalha/Chefao.java
+++ Batalha/src/Batalha/Chefao.java
@@ -1,4 +1,20 @@
 package Batalha;
 
+
+import java.util.Random;
+
 public class Chefao {
+
+    Random rd = new Random();
+
+    
+
+    private boolean calcularChance(){
+        //
+        int  chanceDeAcerto = rd.nextInt(3);
+
+        if (chanceDeAcerto == 1) {
+            return true;
+        }else return false;
+    }
 }
